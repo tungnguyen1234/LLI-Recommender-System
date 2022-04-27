75cae01e50cd5ea6c73137e44ce9d3ae348876be
Sang T. Truong <sttruong@cs.stanford.edu>
2022-04-27 10:09:59 -0700

    parsing + documentation

--- tensor_movieLens/tensor_movieLens.py
+++ tensor_movieLens/tensor_movieLens.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+'''
+File descriptions
+'''
+
+__author__      = 'Tung Nguyen, Sang Truong'
+__copyright__   = 'Copyright 2022, University of Missouri, Stanford University'
+
+from argparse import Namespace, 
 import pandas as pd
 import numpy as np
 from numpy import *
@@ -6,8 +15,25 @@ from matrix_movieLens import matrix_rating
 from tensor_3D_latent import tensor_latent
 
 
-'''This function extracts the age and occupation features'''
+parser = ArgumentParser()
+
+# general arguments
+parser.add_argument("--age", choices=("True", "False"), default="True")
+
+args = parser.parse_args()
+args.amortized_vi = True if args.amortized_vi == "True" else False
+
+
 def extract_3D_dataset(limit = None):
+    '''
+    Desciption:
+        Extracts the age and occupation features
+    Input:
+        limit: TODO: give the type of limit (int, bool, string?) and describe what it does
+    Output:
+        similar to input
+    '''
+    
     # csv_movies = pd.read_csv('movies.csv')
     csv_users = pd.read_csv('users.csv', names = ["UserID", "Gender","Age","Occupation", "Zip-code"])
     df = pd.DataFrame(csv_users)
@@ -145,26 +171,3 @@ def tensor_traintest_score(tensor, percent):
     MAE = float(MAE/len(test))
     MSE = float(MSE/len(test))
     return MAE, MSE, errors
-
-
-if __name__ == '__main__':
-    matrix_rating = matrix_rating()
-    ages, occupations = extract_3D_dataset(900)
-    
-    # matrix_rating = np.array([[1, 1, 0], [0, 0, 2], [3, 3, 4]])
-    # ages = np.array([1, 20, 30])
-    # occupations = np.array([0, 4, 5, 6])
-    # print(len(ages))
-
-    tensor_rating = tensor_age(matrix_rating, ages)
-    # print(tensor_rating)
-    # tensor_rating = tensor_occupation(matrix_rating, occupations)
-    # print(tensor_rating)
-    # tensor_rating = tensor_age_occup(matrix_rating, ages, occupations)
-    # print(tensor_rating)
-    
-    
-    MAE, MSE, errors = tensor_traintest_score(tensor_rating, percent= 0.2)
-    print("MAE is", round(MAE, 2))
-    print("MSE is", round(MSE, 2))
-    print("Error intervals has", errors)
