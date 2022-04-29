f8c4114fecae35d9ac331e3c07056913831a1f9b
Sang T. Truong <sttruong@cs.stanford.edu>
2022-04-28 19:25:00 -0700

    Update tensor_retrieve.py

--- src/tensor_retrieve.py
+++ src/tensor_retrieve.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+'''
+File descriptions
+'''
+
+__author__      = 'Tung Nguyen, Sang Truong'
+__copyright__   = 'Copyright 2022, University of Missouri, Stanford University'
 
 import pandas as pd
 import numpy as np
