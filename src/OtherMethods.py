1763910d33122d7a571b6e4815bbd8c4ca3dafff
Tung Nguyen <tungnguyen211098@gmail.com>
2022-05-10 18:16:13 -0500

    modified:   src/OtherMethods.py

--- src/OtherMethods.py
+++ src/OtherMethods.py
@@ -1,10 +1,7 @@
-import random
 import numpy as np
-from surprise import Dataset, accuracy, Reader
-from surprise.model_selection import train_test_split, KFold
-from surprise import SVD, NMF, SlopeOne, NormalPredictor, KNNBasic, KNNWithMeans, KNNWithZScore, KNNBaseline
-
-import os
+# from surprise import Dataset, accuracy, Reader
+# from surprise.model_selection import train_test_split, KFold
+# from surprise import SVD, NMF, SlopeOne, NormalPredictor, KNNBasic, KNNWithMeans, KNNWithZScore, KNNBaseline
 
 
 def other_methods(percent, dataname, other_method):
